0f5d4716d70623c148ca8c6f66e1406e8d1bd762
tobyatgithub <toby.fangyuan@gmail.com>
2018-12-03 17:37:32 -0800

    close() problem

--- client.py
+++ client.py
@@ -1,3 +1,6 @@
+import uuid
+import random
+
 class Client:
     """ """
     def __init__(self, conn=None, addr=None):
@@ -5,10 +8,18 @@ class Client:
         self.nick = f'user_{random.random()}'
         self.conn = conn
         self.addr = addr
-    
+
     def __str__(self):
-        pass
+        output = f'this is Client with id = { self.id }, nick name = { self.nick }, \nconnection = { self.conn }, \nand address = { self.addr }.'
+        return output
 
     def __repr__(self):
-        pass
+        output = f'this is Client with id = { self.id }, nick name = { self.nick }, \nconnection = { self.conn }, \nand address = { self.addr }.'
+        return output
 # need a way to change use nickname
+    def change_nickname(self, new_nick):
+        try:
+            self.nick = f'{ str(new_nick) }'
+            return
+        except:
+            pass
--- server.py
+++ server.py
@@ -12,11 +12,12 @@ class ChatServer(threading.Thread):
         self.port = port
         self.host = host
         self.server = socket.socket(
-            socket.AF_INET, 
+            socket.AF_INET,
             socket.SOCK_STREAM,
             socket.IPPROTO_TCP,
         )
         self.client_pool = []
+        self.message_history = {}
 
         try:
             self.server.bind((self.host, self.port))
@@ -25,18 +26,46 @@ class ChatServer(threading.Thread):
 
         self.server.listen(10)
 
+
     def run_thread(self, id, nick, conn, addr):
         """ """
         print(f'{nick}Connected rcvd for {addr[0]}{addr[1]}')
 
         while True:
             data = conn.recv(4096)
-            # if len(self.client_pool):
-            #     for c in self.client_pool:
-            #         c.conn.sendall(data)
-            [c.conn.sendall(data) for c in self.client_pool if len(self.client_pool)]
+            import pdb; pdb.set_trace()
+            if len(self.client_pool):
+                for c in self.client_pool:
+                    if c.id not in self.message_history.keys():
+                        self.message_history[c.id] = []
 
-            # ?rather than a list comprehension, call a parse() method that handles all of the message parsing and client communications
+                    # print("c = ",c , "...\n\n and type(c) = \n\n", type(c))
+                    # print("type of data = ", type(data), "\n\ndata content = ", str(data), "\n\n")
+                    # print("decoded data = ", data.decode(), "\n\n")
+                    # output = "for client " + str(c) + "we send data to all"
+                    # print(output)
+                    # c.conn.sendall(data)
+
+                    self.message_history[c.id].append(data.decode())
+                    # import pdb; pdb.set_trace()
+                    if data == b'/quit\n':
+                        if c.id == id:
+                            c.conn.close()
+                    else:
+                        c.conn.sendall(data)
+            # [c.conn.sendall(data) for c in self.client_pool if len(self.client_pool)]
+
+            # ?rather than a list comprehension, call a parse() method that handles
+            # all of the message parsing and client communications
+
+    def parse(in_info):
+        """
+        this function shall handles all of the message parsing and client communications
+        """
+        # if
+        pass
+
+
     def run(self):
         """ """
         print(f'Server running on {self.host}{self.port}.')
@@ -55,6 +84,7 @@ class ChatServer(threading.Thread):
 if __name__ == '__main__':
     server = ChatServer(PORT)
 
+    # server.run()
     try:
         server.run()
     except KeyboardInterrupt:
