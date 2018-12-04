c65e9925c835b65628ed55999911be342a9c5b53
mthwbrwn <mthwbrwn@gmail.com>
2018-12-03 16:03:46 -0800

    starter code added

---
+++ client.py
@@ -0,0 +1,14 @@
+class Client:
+    """ """
+    def __init__(self, conn=None, addr=None):
+        self.id = str(uuid.uuid4())
+        self.nick = f'user_{random.random()}'
+        self.conn = conn
+        self.addr = addr
+    
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
+# need a way to change use nickname
